6ffb4a80ace34ea4da21ac7e0844167ffeaabf32
Matheus Borges <mattborgesdev@gmail.com>
2025-07-10 23:48:42 -0300

    feat: add about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,19 @@ export default function AboutPage() {
       </h1>
       <article>
         <p>
-          Coming soon.
+          I'm Matheus Borges, an SRE/DevOps engineer with experience in infrastructure automation, cloud computing, and CI/CD best practices. I work with tools like <b>Terraform, Ansible, Packer, Docker, and GitHub Actions</b> to turn infrastructure into reliable, scalable, and maintainable code.
+        </p>
+        <p>
+          My journey began by managing a LAN house, where I developed both technical and <b>interpersonal skills</b> while handling customer service and performing computer and systems maintenance.
+        </p>
+        <p>
+          That early experience sparked my interest in technology and led me to enroll in a Technical Program in Informatics, where <b>I wrote my first lines of code using Java</b> and other tools.
+        </p>
+        <p>
+          Over time, I earned my first internship working with PHP, and I <b>currently work at TOTVS</b>, developing automated solutions for a wide range of challenges.
+        </p>
+        <p>
+          I am constantly evolving, currently pursuing a degree in Computer Science and continuously learning, guided by the philosophy of <b>lifelong learning</b>.
         </p>
       </article>
     </>
